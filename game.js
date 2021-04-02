6812baff58b672962f2b4e6c0431143ae14a80ae
majidsawadi <majidsawadi.fusion@gmail.com>
2021-04-02 18:26:35 +0900

    the clone of the game

--- game.js
+++ game.js
@@ -1,13 +1,4 @@
-/*
-  Code modified from:
-  http://www.lostdecadegames.com/how-to-make-a-simple-html5-canvas-game/
-  using graphics purchased from vectorstock.com
-*/
 
-/* Initialization.
-Here, we create and add our "canvas" to the page.
-We also load all of our images.
-*/
 
 const canvas = document.createElement('canvas');
 const ctx = canvas.getContext('2d');
@@ -17,15 +8,7 @@ document.getElementById('canvas').appendChild(canvas);
 
 let bg = {};
 
-/**
- * Setting up our characters.
- *
- * Note that hero.x represents the X position of our hero.
- * hero.y represents the Y position.
- * We'll need these values to know where to "draw" the hero.
- * The same goes for the monsters
- *
- */
+
 
 let hero = { x: canvas.width / 2, y: canvas.height / 2 };
 let monsters = [
@@ -63,16 +46,10 @@ function loadImages() {
 	});
 }
 
-/**
- * Keyboard Listeners
- * You can safely ignore this part, for now.
- *
- * This is just to let JavaScript know when the user has pressed a key.
- */
+
 let keysPressed = {};
 function setupKeyboardListeners() {
-	// Check for keys pressed where key represents the keycode captured
-	// For now, do not worry too much about what's happening here.
+	
 	document.addEventListener(
 		'keydown',
 		function (e) {
@@ -90,14 +67,9 @@ function setupKeyboardListeners() {
 	);
 }
 
-/**
- *  Update game objects - change player position based on key pressed
- *  and check to see if the monster has been caught!
- *
- *  If you change the value of 5, the player will move at a different rate.
- */
+
 let update = function () {
-	// Update the time.
+	
 	elapsedTime = Math.floor((Date.now() - startTime) / 1000);
 
 	if (keysPressed['ArrowUp']) {
@@ -113,21 +85,18 @@ let update = function () {
 		hero.x += 5;
 	}
 
-	// Check if player and monster collided. Our images
-	// are 32 pixels big.
+	
 	monsters.forEach((monster) => {
 		if (hero.x <= monster.x + 32 && monster.x <= hero.x + 32 && hero.y <= monster.y + 32 && monster.y <= hero.y + 32) {
-			// Pick a new location for the monster.
-			// Note: Change this to place the monster at a new, random location.
+			
 			monster.x = monster.x + 50;
 			monster.y = monster.y + 70;
+	
 		}
 	});
 };
 
-/**
- * This function, render, runs as often as possible.
- */
+
 function render() {
 	if (bg.ready) {
 		ctx.drawImage(bg.image, 0, 0);
@@ -139,29 +108,40 @@ function render() {
 		if (monster.ready) {
 			ctx.drawImage(monster.image, monster.x, monster.y);
 		}
+		const monsterCaughtByHero =
+hero.x <= monster.x + 32 &&
+monster.x <= hero.x + 32 &&
+hero.y <= monster.y + 32 &&
+monster.y <= hero.y + 32;
+if (monsterCaughtByHero) {
+monster.x = randomlyPlace("x");
+monster.y = randomlyPlace("y");
+}
 	});
 	ctx.fillText(`Seconds Remaining: ${SECONDS_PER_ROUND - elapsedTime}`, 20, 100);
 }
+function randomlyPlace(axis) {
+	if (axis === "x") {
+	  return Math.floor(Math.random() * canvas.width + 1);
+	} else {
+	  return Math.floor(Math.random() * canvas.height + 1);
+	}
+  }
+  
 
-/**
- * The main game loop. Most every game will have two distinct parts:
- * update (updates the state of the game, in this case our hero and monster)
- * render (based on the state of our game, draw the right things)
- */
 function main() {
 	update();
 	render();
-	// Request to do this again ASAP. This is a special method
-	// for web browsers.
+	
+
 	requestAnimationFrame(main);
 }
 
-// Cross-browser support for requestAnimationFrame.
-// Safely ignore this line. It's mostly here for people with old web browsers.
+
 var w = window;
 requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
 
-// Let's play this game!
+
 loadImages();
 setupKeyboardListeners();
 main();
